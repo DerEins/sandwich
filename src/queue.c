f528dc0ba3e57d52da881d2e4d493d3a48bb125f
DUPOUX Mathieu <Mathieu.Dupoux@bordeaux-inp.fr>
2021-11-09 22:08:27 +0100

    Ajout d'une première implémentation de file de changements entre chaque itération.

--- src/queue.c
+++ src/queue.c
@@ -1,1 +1,64 @@
 #include "queue.h"
+#include "rule.h"
+#include <stdio.h>
+
+/** Concrete struct representing a queue of modifications which has to be done on a world */
+struct change {
+    int x;
+    int y;
+    int idx_rule;
+    struct change* next;
+};
+
+struct queue {
+    int len_queue;
+    struct change list_changes[WIDTH * HEIGHT];
+    struct change* first_to_do;
+    struct change* first_done;
+};
+
+struct change* queue_append(struct queue* queue, int idx_rule, int x, int y)
+{
+    ++(queue->len_queue);
+    struct change* change = &(queue->list_changes[queue->len_queue]);
+    change->idx_rule = idx_rule;
+    change->x = x;
+    change->y = y;
+    change->next = NULL;
+    return change;
+}
+
+void queue_init(struct queue* queue)
+{
+    queue->len_queue = 0;
+    queue->first_to_do = NULL;
+    queue->first_done = NULL;
+}
+
+void change_add(struct queue* queue, struct change* change)
+{
+    if (queue->first_to_do == NULL) {
+        queue->first_to_do = change;
+    } else {
+        struct change* change_tmp = queue->first_to_do;
+        queue->first_to_do = change;
+        change->next = change_tmp;
+    }
+}
+
+void change_remove(struct queue* queue, struct change* change)
+{
+    struct change* previous_change = queue->first_to_do;
+    while (previous_change->next != change) {
+        previous_change = previous_change->next;
+    }
+    previous_change->next = change->next;
+
+    if (queue->first_done == NULL) {
+        queue->first_done = change;
+    } else {
+        struct change* change_tmp = queue->first_done;
+        queue->first_done = change;
+        change->next = change_tmp;
+    }
+}
--- src/queue.h
+++ src/queue.h
@@ -3,6 +3,25 @@
 
 #include "world.h"
 
+/** Abstract struct representing a queue of moodifications */
 struct queue;
+
+/** Abstract struct representing a modicifation inside the queue */
+struct change;
+
+/** Create a new change */
+struct change* queue_append(struct queue* queue, int x, int y, int idx_rule);
+
+/** Create a new modifications queue*/
+void queue_init(struct queue* queue);
+
+/** Add a modificatvoidion to an existing queue*/
+void change_add(struct queue* queue, struct change* change);
+
+/** Remove a change to a queue*/
+void change_rm(struct queue* queue, struct change* change);
+
+/** Apply a modifications queue to an existing world*/
+void change_apply(struct queue* queue, struct world* world);
 
 #endif // __QUEUE_H__
